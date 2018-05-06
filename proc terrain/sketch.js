1d3ae8d9ce9cc8ebc21ee0c23e3af24ed64849c2
victor cesario <victor.cesario@hotmail.com>
2018-05-06 16:46:38 -0300

    values on proc terrain.

--- proc terrain/sketch.js
+++ proc terrain/sketch.js
@@ -1,11 +1,10 @@
 var cols, rows, scl, terrain, xoff, yoff, flying, flySpeed, hue, hueSpeed;
 
 function setup() {
-
 	createCanvas(600, 600, WEBGL);
 	colorMode(HSB);
 	
-	scl = 20;
+	scl = 15;
 	cols = width / scl;
 	rows = height / scl;
 
@@ -17,17 +16,16 @@ function setup() {
 	xoff = 0;
 	yoff = 0;
 	flying = 0;
-	flySpeed = -0.04;
+	flySpeed = -0.02;
 	hue = 0;
 	hueSpeed = .21;
-
 }
 
 function draw() {
 	background(0);
 	stroke(color(hue, 50, 80, 1));
 
-	translate(-width / 2 + 10, -height / 4, 0);
+	translate(-width / 2, -height / 4, 0);
 	rotateX(PI / 3);
 
 	ambientMaterial(0, 0, 0, 0);
@@ -38,18 +36,16 @@ function draw() {
 		yoff = flying;
 		for (var y = 0; y < rows; y++) {
 			terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-			yoff += 0.1;
+			yoff += 0.05;
 		}
-		xoff += 0.1;
+		xoff += 0.05;
 	}
 
 	for (var y = 0; y < rows - 1; y++) {
 		beginShape(TRIANGLE_STRIP);
-		for (var x = 0; x < cols - 1; x ++) {
+		for (var x = 0; x < cols; x ++) {
 			var a = [x * scl, y * scl, terrain[x][y]];
 			var b = [x * scl, (y + 1) * scl, terrain[x][y + 1]];
-			var c = [(x + 1) * scl, (y + 1) * scl, terrain[x + 1][y + 1]];
-			var d = [(x + 1) * scl, y * scl, terrain[x + 1][y]];
 
 			vertex(a[0], a[1], a[2]);
 			vertex(b[0], b[1], b[2]);
